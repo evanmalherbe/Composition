c4f820194ee5dff3aa88b3c34027d4cb86e379f8
evanmalherbe <evanmalherbe@gmail.com>
2022-12-16 08:55:45 +0200

    Add project files.

---
+++ Composition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition
+{
+   
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DbMigrator dbMigrator = new DbMigrator(new Logger());
+            Installer installer = new Installer(new Logger());
+
+            dbMigrator.Migrate();
+            installer.Install();
+        }
+    }
+}
